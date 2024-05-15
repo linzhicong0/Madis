8ba54e22fb53327debef4195abf4488b836ac59e
Jack Lin <jackzlin@deloitte.com.cn>
2024-05-15 22:09:46 +0800

    feat: * create the close button in the connection configuration page * add the connection configuration to the list

--- Madis/Views/Connection/ConnectionConfigurationView.swift
+++ Madis/Views/Connection/ConnectionConfigurationView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ConnectionConfigurationView: View {
+    
+    @Environment(\.appViewModel) private var appViewModel
+    
     @Binding var showDialog: Bool
     @State private var connectionName: String = ""
     @State private var connectionHost: String = "127.0.0.1"
@@ -15,64 +18,102 @@ struct ConnectionConfigurationView: View {
     @State private var username: String = ""
     @State private var password: String = ""
     
+    @State private var closeButtonHovering = false
     
     var body: some View {
         
-        VStack(spacing: 20) {
-            
-            Text("New Connection")
-                .font(.title)
-            
-            // Connection Name
-            CustomFormInputView(title: "Connection Name", systemImage: "list.bullet.circle", placeholder: "connection name", text: $connectionName)
-            
-            // Connection host and port
-            HStack() {
-                CustomFormInputView(title: "Host", systemImage: "house.circle",placeholder: "host" , text: $connectionHost)
-                Text(":")
+        ZStack(alignment: .topLeading) {
+            VStack(spacing: 20) {
+                
+                Text("New Connection")
                     .font(.title)
-                    .offset(y: 10)
-                CustomFormInputView(title: "Port", systemImage: "door.left.hand.closed", placeholder: "port", text: $connectionPort)
-            }
-            HStack {
-                CustomFormInputView(title: "Username", systemImage: "person.crop.circle", placeholder: "username", text: $username)
-                CustomFormInputView(title: "Password", systemImage: "key.horizontal.fill", isSecured: true, placeholder: "password", text: $password)
-            }
-            
-            Spacer()
-            
-            HStack {
+                
+                // Connection Name
+                CustomFormInputView(title: "Connection Name", systemImage: "list.bullet.circle", placeholder: "connection name", text: $connectionName)
+                
+                // Connection host and port
+                HStack() {
+                    CustomFormInputView(title: "Host", systemImage: "house.circle",placeholder: "host" , text: $connectionHost)
+                    Text(":")
+                        .font(.title)
+                        .offset(y: 10)
+                    CustomFormInputView(title: "Port", systemImage: "door.left.hand.closed", placeholder: "port", text: $connectionPort)
+                }
+                HStack {
+                    CustomFormInputView(title: "Username", systemImage: "person.crop.circle", placeholder: "username", text: $username)
+                    CustomFormInputView(title: "Password", systemImage: "key.horizontal.fill", isSecured: true, placeholder: "password", text: $password)
+                }
+                
                 Spacer()
-                Button(action: {}, label: {
-                    Text("Test Connection")
-                        .padding(5)
-                        .foregroundStyle(.white)
-                        .buttonStyle(.borderedProminent)
-                })
                 
-                Button(action: {
-                    showDialog = false
-                }, label: {
-                    Text("Cancel")
-                        .padding(5)
-                        .foregroundStyle(.white)
+                HStack {
+                    Spacer()
+                    Button(action: {}, label: {
+                        Text("Test Connection")
+                            .padding(5)
+                            .foregroundStyle(.white)
+                            .buttonStyle(.borderedProminent)
+                    })
                     
-                })
-                .buttonStyle(.borderedProminent)
+                    Button(action: {
+                        showDialog = false
+                    }, label: {
+                        Text("Cancel")
+                            .padding(5)
+                            .foregroundStyle(.white)
+                        
+                    })
+                    .buttonStyle(.borderedProminent)
+                    
+                    Button( action: {
+                        
+                        let newConnection = Connection(name: self.connectionName, host: self.connectionHost, port: self.connectionPort, username: self.username, password: self.password)
+                        
+                        appViewModel.connections.append(newConnection)
+                        
+                        close()
+                        
+                    }, label: {
+                        Text("Create")
+                            .padding(5)
+                            .foregroundStyle(.white)
+                    })
+                    .buttonStyle(.borderedProminent)
+                }
                 
-                Button( action: {}, label: {
-                    Text("Create")
-                        .padding(5)
-                        .foregroundStyle(.white)
-                })
-                .buttonStyle(.borderedProminent)
+                
             }
+            .padding(15)
+            .frame(width: 400, height:400)
+            .background(BlurView())
             
-            
+            // close button
+            Button(action: {
+                close()
+            }, label: {
+                ZStack {
+                    Circle()
+                        .foregroundStyle(.red)
+                        .frame(width:14, height: 14)
+                    
+                    if(closeButtonHovering) {
+                        Image(systemName: "xmark")
+                            .font(.system(size: 8))
+                            .bold()
+                            .foregroundStyle(.black)
+                    }
+                }
+            })
+            .offset(x: 10, y: 10)
+            .buttonStyle(PlainButtonStyle())
+            .onHover(perform: { hovering in
+                closeButtonHovering = hovering
+            })
         }
-        .padding(15)
-        .frame(width: 400, height:400)
-        .background(BlurView())
+    }
+    
+    private func close() {
+        showDialog = false
     }
 }
 
@@ -117,6 +158,8 @@ struct CustomFormInputView: View {
             .padding(.horizontal, horizontalPadding)
             .background(.primary.opacity(0.15))
             .clipShape(.rect(cornerRadius: 10))
+            
+            
         }
     }
     
